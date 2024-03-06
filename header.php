162b5097b1e7b79dce71408875838370e8430c4f
Raniel Ronidel <rannielsronidel77@gmail.com>
2024-03-06 11:58:13 +0800

    fourth commit

--- header.php
+++ header.php
@@ -6,8 +6,9 @@
     <title>Document</title>
     <!-- links -->
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
 </head>
 <body>
     <h1 id="main_title">CRUD APPLICATION IN PHP</h1>
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php include('header.php'); ?>
 <?php include('dbcon.php'); ?>
-        <h2>ALL STUDENTS</h2>
+
+        <div class="box1">
+            <h2>ALL STUDENTS</h2>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">ADD STUDENTS</button>
+        </div>
+        
+
         <table class="table table-hover table-bordered table-striped">
             <thead>
                 <tr>
@@ -34,4 +40,48 @@
                 ?>
             </tbody>
         </table>
+
+
+        <!-- Modal -->
+<form action="">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">ADD STUDENTS</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+            <div class="form-group">
+                <label for="f_name">
+                    First Name
+                </label>
+                <input type="text" name="f_name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="l_name">
+                    First Name
+                </label>
+                <input type="text" name="l_name" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="age">
+                    First Name
+                </label>
+                <input type="text" name="age" class="form-control">
+            </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-success">ADD</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
 <?php include('footer.php'); ?>
